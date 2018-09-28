5daae4eae741a6e8e4434f4befe30c947353ccc1
Jimmi Westerberg <jw@konform.com>
2018-09-28 11:53:55 +0200

    Add sentry to the system
    Added develop sentry uri to .env.example

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -81,6 +81,7 @@ $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
 $app->register(App\Providers\MaconomySerivceProvider::class);
+$app->register(\Sentry\SentryLaravel\SentryLumenServiceProvider::class);
 
 if ($app->environment() !== 'production') {
     $app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
