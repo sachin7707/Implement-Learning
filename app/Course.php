590d52c39cb35ad95a0ed01701928c859b7d125f
Jimmi Westerberg <jw@konform.com>
2018-10-04 13:08:11 +0200

    Added phpdoc using the ide-helper

--- app/Course.php
+++ app/Course.php
@@ -11,6 +11,36 @@ use Illuminate\Database\Eloquent\Model;
  * @since 2018-09-27
  * @mixin \Eloquent
  * @property int $participants_max
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $start_time
+ * @property string $end_time
+ * @property float $price
+ * @property string $maconomy_id
+ * @property int $seats_available
+ * @property int $participants_min
+ * @property int $participants_current
+ * @property string $name
+ * @property string $language
+ * @property string $venue_number
+ * @property string $venue_name
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Order[] $orders
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereEndTime($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereLanguage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereMaconomyId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereParticipantsCurrent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereParticipantsMax($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereParticipantsMin($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereSeatsAvailable($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereStartTime($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereVenueName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Course whereVenueNumber($value)
  */
 class Course extends Model
 {
--- app/Order.php
+++ app/Order.php
@@ -10,6 +10,17 @@ use Illuminate\Database\Eloquent\Model;
  * @author jimmiw
  * @since 2018-09-26
  * @mixin \Eloquent
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $course_id
+ * @property int $seats
+ * @property-read \App\Course $course
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Order whereCourseId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Order whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Order whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Order whereSeats($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Order whereUpdatedAt($value)
  */
 class Order extends Model
 {
