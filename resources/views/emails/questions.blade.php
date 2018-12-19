ff1ce6abaeaf22d0e9a210ce8a722be5d9906bff
Jimmi Westerberg <jw@konform.com>
2018-12-19 16:02:12 +0100

    Add email layout
    Started work on the email layout for basic email template.
    Added a few components to use, when doing the email, to reduce the
    number of repeated lines of code.
    
    ILI-279

---
+++ resources/views/emails/questions.blade.php
@@ -0,0 +1,61 @@
+<!-- questions block start -->
+
+@component('emails.components.spacer', ['color' => '#efddd3'])
+@endcomponent
+
+<div class="layout one-col fixed-width" style="Margin: 0 auto;max-width: 600px;min-width: 320px; width: 320px;width: calc(28000% - 167400px);overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;">
+    <div class="layout__inner" style="border-collapse: collapse;display: table;width: 100%;background-color: #efddd3;">
+        <!--[if (mso)|(IE)]><table align="center" cellpadding="0" cellspacing="0" role="presentation"><tr class="layout-fixed-width" style="background-color: #efddd3;"><td style="width: 600px" class="w560"><![endif]-->
+        <div class="column" style="text-align: left;color: #2f353e;font-size: 15px;line-height: 23px;font-family: Calibri,Carlito,PT Sans,Trebuchet MS,sans-serif;max-width: 600px;min-width: 320px; width: 320px;width: calc(28000% - 167400px);">
+
+            <div style="Margin-left: 20px;Margin-right: 20px;">
+                <div style="mso-line-height-rule: exactly;mso-text-raise: 4px;">
+                    <p class="size-16" style="Margin-top: 0;Margin-bottom: 0;font-size: 16px;line-height: 24px;" lang="x-size-16">Nogen sp&#248;rgsm&#229;l?</p>
+                </div>
+            </div>
+
+        </div>
+        <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+    </div>
+</div>
+
+<div class="layout one-col fixed-width" style="Margin: 0 auto;max-width: 600px;min-width: 320px; width: 320px;width: calc(28000% - 167400px);overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;">
+    <div class="layout__inner" style="border-collapse: collapse;display: table;width: 100%;background-color: #efddd3;">
+        <!--[if (mso)|(IE)]><table align="center" cellpadding="0" cellspacing="0" role="presentation"><tr class="layout-fixed-width" style="background-color: #efddd3;"><td style="width: 600px" class="w560"><![endif]-->
+        <div class="column" style="text-align: left;color: #2f353e;font-size: 15px;line-height: 23px;font-family: Calibri,Carlito,PT Sans,Trebuchet MS,sans-serif;max-width: 600px;min-width: 320px; width: 320px;width: calc(28000% - 167400px);">
+
+            <div style="Margin-left: 20px;Margin-right: 20px;">
+                <div style="mso-line-height-rule: exactly;mso-text-raise: 4px;">
+                    <p class="size-30" style="Margin-top: 0;Margin-bottom: 0;font-size: 26px;line-height: 34px;" lang="x-size-30">Ring til os p&#229; 33 12 20 05 &nbsp;</p>
+                </div>
+            </div>
+
+        </div>
+        <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+    </div>
+</div>
+
+@component('emails.components.spacer', ['color' => '#efddd3'])
+@endcomponent
+
+<div class="layout one-col fixed-width" style="Margin: 0 auto;max-width: 600px;min-width: 320px; width: 320px;width: calc(28000% - 167400px);overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;">
+    <div class="layout__inner" style="border-collapse: collapse;display: table;width: 100%;background-color: #efddd3;">
+        <!--[if (mso)|(IE)]><table align="center" cellpadding="0" cellspacing="0" role="presentation"><tr class="layout-fixed-width" style="background-color: #efddd3;"><td style="width: 600px" class="w560"><![endif]-->
+        <div class="column" style="text-align: left;color: #2f353e;font-size: 15px;line-height: 23px;font-family: Calibri,Carlito,PT Sans,Trebuchet MS,sans-serif;max-width: 600px;min-width: 320px; width: 320px;width: calc(28000% - 167400px);">
+
+            <div style="Margin-left: 20px;Margin-right: 20px;">
+                <div style="mso-line-height-rule: exactly;mso-text-raise: 4px;">
+                    <p class="size-16" style="Margin-top: 0;Margin-bottom: 0;font-size: 16px;line-height: 24px;" lang="x-size-16">Strandvejen 54, 2900 Hellerup, Denmark<br>
+                        Implement Learning Institute</p>
+                </div>
+            </div>
+
+        </div>
+        <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+    </div>
+</div>
+
+@component('emails.components.spacer', ['color' => '#efddd3'])
+@endcomponent
+
+<!-- questions block end -->
