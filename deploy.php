2862fbf501055613ca2caa1233792039a86ee7cc
Jimmi Westerberg <jw@konform.com>
2019-01-18 14:09:05 +0100

    Add deployer to the mix
    Added the initial setup for the lumen (laravel) deploy.
    
    Changed the deploy-task, since it's a lumen project.
    Source: https://github.com/deployphp/deployer/issues/1534
    
    Added a next host.

---
+++ deploy.php
@@ -0,0 +1,44 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+require 'recipe/laravel.php';
+//require 'vendor/konform/recipes/recipe/konform.php';
+
+// Project name
+set('application', 'ilibackend');
+
+// Project repository
+set('repository', 'ssh://git@git.konform.com:7999/ili/ili-backend.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Hosts
+host('next')
+    ->hostname('dev.konform.com')
+    ->stage('develop')
+    ->roles(['db','web'])
+    ->set('branch', 'next')
+    ->set('deploy_path', '/domains/konform/sites/{{application}}');
+
+// default stage host is selected as next
+set('default_stage', 'next');
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'artisan:cache:clear',
+    'artisan:optimize',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
