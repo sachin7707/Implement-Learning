b88721302e81362e6669fe4452ca8cbef3bb88a3
Jimmi Westerberg <jw@konform.com>
2019-01-31 10:46:41 +0100

    You should be able to create and update mail texts
    Added a new table migration.
    Added a model, with a few helper methods.
    Updated controller to save the data given.
    
    ILI-444

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\MailText;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 
@@ -16,7 +17,34 @@ class MailController extends Controller
      */
     public function update(Request $request)
     {
-        $data = $request->input();
-        Log::info('MailController::update: ' . print_r($data,1));
+        $this->validate($request, [
+            'texts' => 'required|array',
+            'lang' => 'required|string'
+        ]);
+        $texts = $request->input('texts');
+        $language = $request->input('lang');
+
+        foreach ($texts as $type => $text) {
+            // if the type is not valid, just skip it
+            if (! MailText::isTypeValid($type)) {
+                continue;
+            }
+
+            // fetching and updating exsisting mailtype
+            $mailText = MailText::getByTypeAndLanguage($type, $language);
+
+            if ($mailText) {
+                $mailText->text = $text;
+                $mailText->save();
+            } else {
+                // new MailText, create a new instance on the given language
+                $mailText = new MailText();
+                $mailText->save([
+                    'type' => $type,
+                    'text' => $text,
+                    'language' => $language
+                ]);
+            }
+        }
     }
 }
---
+++ app/MailText.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @author jimmiw
+ * @since 2019-01-31
+ */
+class MailText extends Model
+{
+    const TYPE_DEFAULT_PARTICIPANT = 'default_participant_mail';
+    const TYPE_DEFAULT_WELCOME_HOME = 'default_welcome_home_mail';
+    const TYPE_REMINDER = 'remindermail';
+    const TYPE_MAIL_FOOTER = 'mailfooter';
+
+    /**
+     * Fetches the mail text, using the given $type
+     * @param string $type the mail type, see TYPE_X constants
+     * @param string $language the language code to get the text for (ISO-2 language code)
+     * @return MailText|null the found mailtext or null
+     */
+    public static function getByTypeAndLanguage(string $type, string $language)
+    {
+        return self::where('type', $type)
+            ->where('language', $language)
+            ->first();
+    }
+
+    /**
+     * Checks if the type is valid
+     * @param string $type
+     * @return bool
+     */
+    public static function isTypeValid(string $type)
+    {
+        return in_array($type, [
+            self::TYPE_DEFAULT_PARTICIPANT,
+            self::TYPE_DEFAULT_WELCOME_HOME,
+            self::TYPE_MAIL_FOOTER,
+            self::TYPE_REMINDER
+        ]);
+    }
+}
