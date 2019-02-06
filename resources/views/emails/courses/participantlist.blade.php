853144dc33cb3310a44c8b11bdd9b6101d73ee29
Jimmi Westerberg <jw@konform.com>
2019-02-06 14:46:49 +0100

    Add what variables the frontender can work with
    Added $trainer and $daysTo in a comment in the blade file.
    
    ILI-230

--- resources/views/emails/courses/participantlist.blade.php
+++ resources/views/emails/courses/participantlist.blade.php
@@ -1,7 +1,12 @@
-@extends('layouts.email', ['course' => $course, 'footer' => json_decode($footer->text)])
+@extends('layouts.email', [
+    'course' => $course,
+    'footer' => json_decode($footer->text)
+])
 @section('title', 'Deltagerliste')
 
 @section('participantlist')
     we just need some data here... this is not displayed anywhere though :)
+
+    // NOTE: $trainer + $daysTo are also available
 
 @endsection
