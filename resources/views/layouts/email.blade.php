c7e7984d3079d1001031010959c8ebe6eb11d70c
Jimmi Westerberg <jw@konform.com>
2019-03-08 12:13:11 +0100

    Some error char was added?
    ILI-584

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -787,7 +787,7 @@
                                                     </tr>
                                                     <tr class="size-15" style="Margin-top: 20px;Margin-bottom: 0;font-size: 15px;line-height: 23px;" lang="x-size-15">
                                                         <td style="color:#8e8e8e; width: 25%"></td>
-                                                        <td style="color:#000000">{{ $language === 'da' ? 'tlf.' : 'Phone' }} {{ $participant->phone }}</td>
+                                                        <td style="color:#000000">{{ $language === 'da' ? 'tlf.' : 'Phone' }} {{ $participant->phone }}</td>
                                                         <td style="color:#000000"><a style="text-decoration: underline;transition: opacity 0.1s ease-in;color: #000;" href="mailto: {{ $participant->email }}">{{ $participant->email }}</a></td>
                                                     </tr>
                                                 </table>
