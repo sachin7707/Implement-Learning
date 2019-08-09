acbde1f37ed9e3c74b285a4363eeed94144b5d08
Jimmi Westerberg <jw@konform.com>
2019-08-09 11:33:22 +0200

    Add a new mailtype so we can accept default_mail_body from wp
    ILI-734

--- app/MailText.php
+++ app/MailText.php
@@ -12,6 +12,7 @@ class MailText extends Model
 {
     const TYPE_DEFAULT_PARTICIPANT = 'default_participant_mail';
     const TYPE_DEFAULT_PARTICIPANT_BEFORE_COURSE = 'default_participant_mail_before_course';
+    const TYPE_DEFAULT_BODY = 'default_mail_body';
     const TYPE_WAITINGLIST_PARTICIPANT = 'default_waitinglist_participant_mail';
     const TYPE_DEFAULT_WELCOME_HOME = 'default_welcome_home_mail';
     const TYPE_REMINDER = 'remindermail';
@@ -42,6 +43,7 @@ class MailText extends Model
         return in_array($type, [
             self::TYPE_DEFAULT_PARTICIPANT,
             self::TYPE_DEFAULT_PARTICIPANT_BEFORE_COURSE,
+            self::TYPE_DEFAULT_BODY,
             self::TYPE_WAITINGLIST_PARTICIPANT,
             self::TYPE_DEFAULT_WELCOME_HOME,
             self::TYPE_MAIL_FOOTER,
